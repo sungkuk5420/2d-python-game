ca58a828248e3abe5c080a2832638689c700b4bb
sungkuk5420 <sungkuk5420@gmail.com>
2020-07-28 01:45:51 +0900

    프레임, 캐릭터 이동속도 변수로 조정가능하도록 수정

--- 1_create_frame.py
+++ 1_create_frame.py
@@ -36,6 +36,9 @@ to_x = 0
 to_y = 0
 
 counter = 0
+
+character_speed = 0.4
+
 running = True
 keydownEventType = "stop"
 while running:
@@ -48,20 +51,20 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 keydownEventType = "left"
-                to_x -= 8.5
+                to_x -= character_speed 
                 
             elif event.key == pygame.K_RIGHT:
                 keydownEventType = "right"
-                to_x += 8.5
+                to_x += character_speed
                 
             # elif event.key == pygame.K_UP:
             #     to_y -= 5
             # elif event.key == pygame.K_DOWN:
             #     to_y += 5
         if keydownEventType =="left":
-            to_x = -8.5
+            to_x = -character_speed
         elif keydownEventType =="right":
-            to_x = 8.5
+            to_x = character_speed
         if event.type == pygame.KEYUP:
             if (event.key == pygame.K_LEFT and keydownEventType=="left") or (event.key == pygame.K_RIGHT and keydownEventType=="right"):
                 keydownEventType = "stop"
@@ -71,8 +74,8 @@ while running:
             # elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
             #     to_y = 0
 
-    character_x_pos += to_x
-    character_y_pos += to_y
+    character_x_pos += to_x * dt
+    character_y_pos += to_y * dt
     if(keydownEventType == "left"):
         counter = (counter + 1) % len(character_left)
         character = pygame.image.load(character_left[counter])
